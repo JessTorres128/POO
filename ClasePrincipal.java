cb3b2080de6c9271220003095469459aef81bf5f
JessTorres128 <jesseatorres128@gmail.com>
2021-03-08 14:37:37 -0700

    HolaMundo

---
+++ ClasePrincipal.java
@@ -0,0 +1,17 @@
+public class ClasePrincipal {
+
+    public static void main(String[]args){
+      short x1=1;
+      int x2=6;
+      short x3=12;
+      boolean entro=false;
+      String texto= "Hola mundo ";
+
+      int numero1=10;
+      int numero2=6;
+      int res=numero2++*2;
+        System.out.println(++numero2);
+        System.out.println(numero2);
+    }
+
+}
